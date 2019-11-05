b035c2353b40c7adf5910ec9bbcd9c7365938144
Youggls <462046122@qq.com>
2019-11-06 00:39:03 +0800

    Fix symbol table implement, user tree structure to descirbe symbol table.

--- common/symbol.cpp
+++ common/symbol.cpp
@@ -18,3 +18,28 @@ const std::string symbol::getIdName() {
 symbolType& symbol::getIdType() {
 	return this->idType;
 }
+
+SymbolTable::SymbolTable() {
+	this->childTable = NULL;
+	this->parentTable = NULL;
+}
+
+SymbolTable::SymbolTable(SymbolTable* parent) {
+	this->childTable = NULL;
+	this->parentTable = parent;
+	parent->setChild(this);
+}
+
+symbol* SymbolTable::findSymbol(std::string name) {
+	std::unordered_map<std::string, symbol*>::iterator iter;
+	iter = this->symbolHashTable.find(name);
+	if (iter != this->symbolHashTable.end()) return iter->second;
+	else return NULL;
+}
+
+int SymbolTable::addSymbol(symbol* symbol) {
+	if (this->findSymbol(symbol->getIdName()) == NULL) {
+		this->symbolHashTable[symbol->getIdName()] = symbol;
+		return SUCCESS;
+	} else return FAIL;
+}
