0d2f23d122a79947c7eaa8334197c4f7292eeb16
Youggls <462046122@qq.com>
2019-12-11 22:04:39 +0800

    Add asm generator class file and asm code marco.

---
+++ common/util/AsmCode.h
@@ -0,0 +1,30 @@
+#ifndef ASMCODE_H
+#define ASMCODE_H
+
+// Asm instructor
+#define MOV     "mov"
+#define ADD     "add"
+#define SUB     "sub"
+#define MUL     "mul"
+#define DIV     "div"
+#define PUSH    "push"
+#define POP     "pop"
+#define CALL    "call"
+#define RET     "ret"
+#define CMP     "cmp"
+
+// 32-bit asm register
+#define EAX     "eax"
+#define EBX     "ebx"
+#define ECX     "ecx"
+#define EDX     "edx"
+#define EBP     "ebp"
+#define ESP     "esp"
+
+// Other characters
+#define LB      "["
+#define RB      "]"
+#define COMMA   ","
+#define COLON   ":"
+
+#endif
---
+++ common/util/AsmGenerator.h
@@ -0,0 +1,11 @@
+#ifndef ASMGENERATOR_H
+#define ASMGENERATOR_H
+#include "./AsmCode.h"
+class AsmGenerator
+{
+private:
+public:
+    AsmGenerator();
+};
+
+#endif
