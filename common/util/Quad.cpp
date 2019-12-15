e4dc66de3ce8318e8baa356a8812bf1e23229a3d
Azerrroth <732814312@qq.com>
2019-12-15 21:34:55 +0800

    Update: Finish Constructor of Quad. (Maybe finished...

--- common/util/Quad.cpp
+++ common/util/Quad.cpp
@@ -1,4 +1,75 @@
 #include "Quad.h"
 #include <typeinfo>
 
+Quad::Quad(OpCode op, int result)
+{
+    this->op = op;
+    this->result.target = result;
+}
 
+Quad::Quad(OpCode op, symbol *arg1, symbol *result)
+{
+    this->op = op;
+    this->arg1.var = arg1;
+    this->result.var = result;
+}
+
+Quad::Quad(OpCode op, int arg1, symbol *result)
+{
+    this->op = op;
+    this->arg1.target = arg1;
+    this->result.var = result;
+}
+
+Quad::Quad(OpCode op, symbol *arg1, symbol *arg2, symbol *result)
+{
+    this->op = op;
+    this->arg1.var = arg1;
+    this->arg2.var = arg2;
+    this->result.var = result;
+}
+
+Quad::Quad(OpCode op, int arg1, symbol *arg2, symbol *result)
+{
+    this->op = op;
+    this->arg1.target = arg1;
+    this->arg2.var = arg2;
+    this->result.var = result;
+}
+
+Quad::Quad(OpCode op, symbol *arg1, int arg2, symbol *result)
+{
+    this->op = op;
+    this->arg1.var = arg1;
+    this->arg2.target = arg2;
+    this->result.var = result;
+}
+Quad::Quad(OpCode op, int arg1, int arg2, symbol *result)
+{
+    this->op = op;
+    this->arg1.target = arg1;
+    this->arg2.target = arg2;
+    this->result.var = result;
+}
+
+Quad::Quad(OpCode op, symbol *arg1, symbol *arg2, int result)
+{
+    this->op = op;
+    this->arg1.var = arg1;
+    this->arg2.var = arg2;
+    this->result.target = result;
+}
+Quad::Quad(OpCode op, symbol *arg1, int arg2, int result)
+{
+    this->op = op;
+    this->arg1.var = arg1;
+    this->arg2.target = arg2;
+    this->result.target = result;
+}
+Quad::Quad(OpCode op, int arg1, int arg2, int result)
+{
+    this->op = op;
+    this->arg1.target = arg1;
+    this->arg2.target = arg2;
+    this->result.target = result;
+}
