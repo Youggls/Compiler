10dce09a1974ca4c5214d0225a854239685002df
Youggls <462046122@qq.com>
2019-12-17 14:16:01 +0800

    Update asm code class. Easy to generate instructor.Ref #12.

--- common/util/AsmGenerator.cpp
+++ common/util/AsmGenerator.cpp
@@ -1,18 +1,84 @@
 #include "AsmGenerator.h"
 
+std::string AsmCode::transRegister(asmRegister reg) {
+    if (reg == asmRegister::eax) return EAX;
+    else if (reg == asmRegister::ebx) return EBX;
+    else if (reg == asmRegister::ecx) return ECX;
+    else if (reg == asmRegister::edx) return EDX;
+    else if (reg == asmRegister::ebp) return EBP;
+    else if (reg == asmRegister::esp) return ESP;
+    else return "";
+}
+
+void AsmCode::generateBinaryInstructor(std::string insturctor, asmRegister reg1, asmRegister reg2) {
+    this->codeBuffer += insturctor + std::string(" ") + this->transRegister(reg1) 
+                        + "," + this->transRegister(reg2) + "\n";
+}
+
+void AsmCode::generateBinaryInstructor(std::string insturctor, asmRegister reg, std::string var) {
+    this->codeBuffer += insturctor + std::string(" ") + this->transRegister(reg) 
+                        + "," + var + "\n";
+}
+
+void AsmCode::generateUnaryInstructor(std::string instructor, asmRegister reg) {
+    this->codeBuffer += instructor + " " + this->transRegister(reg) + "\n";
+}
+
+void AsmCode::generateUnaryInstructor(std::string instructor, std::string var) {
+    this->codeBuffer += instructor + " " + var + "\n";
+}
+
 AsmCode::AsmCode() {
 
 }
 
 void AsmCode::addCode(std::string code) {
-    this->codeList.push_back(code);
+    this->codeBuffer += code;
+    this->codeBuffer += "\n";
 }
 
-std::ostream& operator<<(std::ostream& os, const AsmCode& asmcode) {
-    for (int i = 0; i < asmcode.codeList.size(); i++) {
-        os << asmcode.codeList[i];
-    }
-    return os;
+void AsmCode::add(asmRegister reg1, asmRegister reg2) {
+    this->generateBinaryInstructor(ADD, reg1, reg2);
+}
+
+void AsmCode::add(asmRegister reg, std::string var) {
+    this->generateBinaryInstructor(ADD, reg, var);
+}
+
+void AsmCode::sub(asmRegister reg1, asmRegister reg2) {
+    this->generateBinaryInstructor(SUB, reg1, reg2);
+}
+
+void AsmCode::sub(asmRegister reg, std::string var) {
+    this->generateBinaryInstructor(SUB, reg, var);
+}
+
+void AsmCode::mul(asmRegister reg1, asmRegister reg2) {
+    this->generateBinaryInstructor(MUL, reg1, reg2);
+}
+
+void AsmCode::mul(asmRegister reg, std::string var) {
+    this->generateBinaryInstructor(MUL, reg, var);
+}
+
+void AsmCode::div(asmRegister reg1, asmRegister reg2) {
+    this->generateBinaryInstructor(DIV, reg1, reg2);
+}
+
+void AsmCode::div(asmRegister reg, std::string var) {
+    this->generateBinaryInstructor(DIV, reg, var);
+}
+
+void AsmCode::push(asmRegister reg) {
+    this->generateUnaryInstructor(PUSH, reg);
+}
+
+void AsmCode::push(std::string var) {
+    this->generateUnaryInstructor(PUSH, var);
+}
+
+void AsmCode::label(std::string label) {
+    this->codeBuffer += label + COLON;
 }
 
 AsmGenerator::AsmGenerator() {
--- common/util/AsmGenerator.h
+++ common/util/AsmGenerator.h
@@ -6,12 +6,41 @@
 #include <fstream>
 #include "./AsmCode.h"
 
+enum class asmRegister {
+    eax = 1,
+    ebx = 2,
+    ecx = 3,
+    edx = 4,
+    esp = 5,
+    ebp = 6
+};
+
 class AsmCode {
 private:
-    std::vector<std::string> codeList;
+    std::string codeBuffer;
+    std::string transRegister(asmRegister reg);
+    void generateBinaryInstructor(std::string instructor, asmRegister reg1, asmRegister reg2);
+    void generateBinaryInstructor(std::string instructor, asmRegister reg, std::string reg2);
+    void generateUnaryInstructor(std::string instructor, std::string var);
+    void generateUnaryInstructor(std::string instructor, asmRegister reg);
 public:
     AsmCode();
-    void addCode(std::string code);
+    // ADD instructor, add reg1, reg2
+    void add(asmRegister reg1, asmRegister reg2);
+    // ADD instructor, add reg, var
+    void add(asmRegister reg, std::string var);
+    void sub(asmRegister reg1, asmRegister reg2);
+    void sub(asmRegister reg, std::string var);
+    void mov(asmRegister reg1, asmRegister reg2);
+    void mov(asmRegister reg, std::string var);
+    void mul(asmRegister reg1, asmRegister reg2);
+    void mul(asmRegister reg, std::string var);
+    void div(asmRegister reg1, asmRegister reg2);
+    void div(asmRegister reg, std::string var);
+    void push(asmRegister reg);
+    void push(std::string var);
+    void label(std::string label);
+    void addCode(std::string code);    
     friend std::ostream& operator<<(std::ostream& os, const AsmCode& asmcode);
 };
 
